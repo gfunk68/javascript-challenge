abd10ff40cdd50864effb8da0dfbfc020133bb47
gfunk68 <gfunk68@yahoo.com>
2020-01-28 21:04:14 -0600

    added clear search button and finished bonus filtering

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -22,16 +22,26 @@ button.on("click", function() {
   var shapeinputElement = d3.select("#shape");
   var shapeValue = shapeinputElement.property("value");
 
+  var filteredData = tableData;
+
   if (!!dateValue) {
-    var filteredData = tableData.filter(day => day.datetime === dateValue);
-  } else if (!!cityValue) {
-    var filteredData = tableData.filter(day => day.city.toLowerCase() === cityValue.toLowerCase());
-  } else if (!!stateValue) {
-    var filteredData = tableData.filter(day => day.state.toLowerCase() === stateValue.toLowerCase());
-  } else if (!!countryValue) {
-    var filteredData = tableData.filter(day => day.country.toLowerCase() === countryValue.toLowerCase());
-  } else if (!!shapeValue) {
-    var filteredData = tableData.filter(day => day.shape.toLowerCase() === shapeValue.toLowerCase());
+    var filteredData = filteredData.filter(day => day.datetime === dateValue);
+  };
+  
+  if (!!cityValue) {
+    var filteredData = filteredData.filter(day => day.city.toLowerCase() === cityValue.toLowerCase());
+  };
+  
+  if (!!stateValue) {
+    var filteredData = filteredData.filter(day => day.state.toLowerCase() === stateValue.toLowerCase());
+  };
+  
+  if (!!countryValue) {
+    var filteredData = filteredData.filter(day => day.country.toLowerCase() === countryValue.toLowerCase());
+  };
+  
+  if (!!shapeValue) {
+    var filteredData = filteredData.filter(day => day.shape.toLowerCase() === shapeValue.toLowerCase());
   };
 
 
@@ -51,6 +61,26 @@ button.on("click", function() {
       })
 
   })
+
+});
+
+var clearbutton = d3.select("#clear-btn");
+
+clearbutton.on("click", function() {
+  var dateinputElement = d3.select("#datetime");
+  var dateValue = dateinputElement.property("value");
+  
+  var cityinputElement = d3.select("#city");
+  var cityValue = cityinputElement.property("value");
+
+  var stateinputElement = d3.select("#state");
+  var stateValue = stateinputElement.property("value");
+
+  var countryinputElement = d3.select("#country");
+  var countryValue = countryinputElement.property("value");
+
+  var shapeinputElement = d3.select("#shape");
+  var shapeValue = shapeinputElement.property("value");
   
   dateinputElement.property("value", "");
   cityinputElement.property("value", "");
